ed0d3ed041cbeebdf6f165e8725412a27bfa9559
Akashos23 <65364908+Akashos23@users.noreply.github.com>
2023-10-12 08:07:07 +0200

    Update
    Supprimer la fonction DefineSizeTab (plus aucune utilité)
    Amélioration du decodage de l'image
    Test avec différentes images

--- SteganographyPicture.py
+++ SteganographyPicture.py
@@ -11,34 +11,34 @@ def SteganographyPicture(image1, image2):
     newImage = newImageRename[0] + "inPictureEncode.PNG"
     if(VerifyPicture(img1.size, img2.size) == False):
         return "L'image " + image2 + " est plus grande que l'image " + image1 + ", on ne peut encoder l'image "+ image2 +" sur l'image " + image1
-    [width1, height1] = img1.size
     [width, height] = img2.size
-    WidthPicture = DefineSizeTab(width1, width)
-    HeightPicture = DefineSizeTab(height1, height)
     for x in range(width):
         for y in range(height):
-            if(WidthPicture == False and HeightPicture == False):
-                [R1,G1,B1] = TransformToBin(imageLoad1[x, y])
-                [R2,G2,B2] = TransformToBin(imageLoad2[x, y])
-                NewR = R1[:4]+R2[:4]
-                NewG = G1[:4]+G2[:4]
-                NewB = B1[:4]+B2[:4]
-                imageLoad1[x,y] = (int(NewR, 2), int(NewG, 2), int(NewB, 2))
+            [R1,G1,B1] = TransformToBin(imageLoad1[x, y])
+            [R2,G2,B2] = TransformToBin(imageLoad2[x, y])
+            NewR = R1[:4]+R2[:4]
+            NewG = G1[:4]+G2[:4]
+            NewB = B1[:4]+B2[:4]
+            imageLoad1[x,y] = (int(NewR, 2), int(NewG, 2), int(NewB, 2))
     img1.save(newImage) 
             
 def decodeSteganographyImage(image, size):
     img = Image.open(image)
+    [widthImage, heightImage] = img.size
     [width, height] = size
     imageLoad = img.load()
     newImageRename = image1.split(".")
     newImage = newImageRename[0] + "inPictureDecode.PNG"
-    for x in range(width):
-        for y in range(height):
-            [R1,G1,B1] = TransformToBin(imageLoad[x, y])
-            NewR = R1[-4:]+"0000"
-            NewG = G1[-4:]+"0000"
-            NewB = B1[-4:]+"0000"
-            imageLoad[x,y] = (int(NewR, 2), int(NewG, 2), int(NewB, 2))
+    for x in range(widthImage):
+        for y in range(heightImage):
+            if(x < width and y < height):
+                [R1,G1,B1] = TransformToBin(imageLoad[x, y])
+                NewR = R1[-4:]+"0000"
+                NewG = G1[-4:]+"0000"
+                NewB = B1[-4:]+"0000"
+                imageLoad[x,y] = (int(NewR, 2), int(NewG, 2), int(NewB, 2))
+            else:
+                imageLoad[x,y] = (255, 255, 255)
     img.save(newImage) 
 
 def TransformToBin(image):
@@ -51,12 +51,6 @@ def TransformToBin(image):
     tabRGBToBin.append(NewGToBin)
     tabRGBToBin.append(NewBToBin)
     return tabRGBToBin
-
-def DefineSizeTab(Size1, Size2):
-    if(Size1 > Size2):
-        return False
-    else:
-        return True
 
 def VerifyPicture(image1Size, image2Size):
     [width1, height1] = image1Size
